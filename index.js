b5d06548ed51eba424a14b0b817060252e5259fe
Luís Henrique <61325756+henrique15775@users.noreply.github.com>
2021-08-05 01:03:59 -0300

    bot funcional com fila de musica

--- index.js
+++ index.js
@@ -8,41 +8,57 @@ counter = 0;
 bot.login("ODYxMjYzNzE0NDI5ODI5MTMy.YOHQVw.4aZ4DvtoUQ-hF2YUZKjls-o4gQY");
 bot.once('ready',() => {
   console.log(`Bot online: ${bot.user.tag}!!`)
-}
+});
 
-);
+const bot_recursivo = (connection) => {
+  if(fila.length > 0){
+  const watcher =  connection.play(
+    ytdl(fila[0], { filter: 'audioonly' }))
+watcher.on('speaking', (response) => {
+      if (response == 0){
+        fila.shift();
+        console.log(fila);
+        verify = false;
+        bot_recursivo(connection);
+      }
+    });
+verify = true;
+    console.log("bora ver se vai dar...");
+  }else{
+    console.log('lista vazia!!!');
+  }
+      };
+
+ 
 bot.on('message', async (message) =>{
 
-  if (message.content.slice(0,5) === '!play' && verify == false){
-
+  if (message.content.slice(0,5) === '!play'){
+    //  fila.push(message.content.slice(6));
       const voiceChannel = message.member.voice.channel;
       const connection = await voiceChannel.join();
 
       console.log(message.content);
+      
+      if(verify === false){
+        fila.push(message.content.slice(6));
+      //fila.push(message.content.slice(6));
+      bot_recursivo(connection,fila);
+      /*const watcher = connection.play(
+          ytdl(fila[0], { filter: 'audioonly' }));
+      watcher.on('speaking', (response) => {
+            if (response == 0){
+              fila.shift();
+              console.log(fila);
+              verify = false;
+              bot_recursivo(voiceChannel,connection,watcher);
+            }
+          });
       verify = true;
-      const watcher = ?connection.play(
-        ytdl(fila[counter], { filter: 'audioonly' }));
-        watcher.on('speaking', (valor) => {
-          if (valor == 1){
-            verify= true
-          }
-
-        })
-
-      /* [kkkk,]
-      const broadcast = bot.voice.createBroadcast();
-      broadcast.play(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { filter: 'audioonly' }));
-      // Play "music.mp3" in all voice connections that the client is in
-      for (const connection of bot.voice.connections.values()) {
-        connection.play(broadcast);*/
-      /*watcher.on('end',() => {
-        voiceChannel.leave();
-        console.log("ACABOOOUUU");
-
-      });*/
+      */
+        }else{
+          fila.push(message.content.slice(6));
+      }
     console.log('uiiiii');
-  }else{
-    fila.push(message.content.slice(6));
   }
 
     });
